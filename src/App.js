a17dde42dd51b637ca7b580cf992c9184d5db395
Davi de Lucca <davislucca@gmail.com>
2020-07-23 23:43:42 -0300

    Listando e adicionando repositórios, e iniciando função de exclusão

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories', {
+      id: "123",
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete(`repositories/${id}`);
+    console.log(response);
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
         <li>
-          Repositório 1
+          Repositório {repositories.map((repo) => {
+            <p>{repo.title}</p>
+          })}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
